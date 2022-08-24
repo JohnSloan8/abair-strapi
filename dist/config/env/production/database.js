768abb9e0120ed8070d55bc2e8acd1fc0e5e2ad3
john <johnsloan1984@gmail.com>
2022-08-24 09:01:14 +0100

    fixed ts error

--- dist/config/env/production/database.js
+++ dist/config/env/production/database.js
@@ -1,18 +1,20 @@
-// import parse = require('pg-connection-string').parse;
-// const config = parse(process.env.DATABASE_URL);
-// export default ({ env }) => ({
-//   connection: {
-//     client: 'postgres',
-//     connection: {
-//       host: config.host,
-//       port: config.port,
-//       database: config.database,
-//       user: config.user,
-//       password: config.password,
-//       ssl: {
-//         rejectUnauthorized: false
-//       },
-//     },
-//     debug: false,
-//   },
-// });
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const pg_connection_string_1 = require("pg-connection-string");
+const config = (0, pg_connection_string_1.parse)(process.env.DATABASE_URL);
+exports.default = ({ env }) => ({
+    connection: {
+        client: "postgres",
+        connection: {
+            host: config.host,
+            port: config.port,
+            database: config.database,
+            user: config.user,
+            password: config.password,
+            ssl: {
+                rejectUnauthorized: false,
+            },
+        },
+        debug: false,
+    },
+});
